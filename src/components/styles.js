32c74e3d4a80a69895ecad3cd4ea0d00bcd9230e
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-13 19:11:57 -0200

    create style file

---
+++ src/components/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    capture: {
+        fontSize: 50,
+        color: 'white'
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    header: {
+        backgroundColor: 'teal'
+    },
+    icons: {
+        color: 'white'
+    }
+
+});
+
+export default styles;
