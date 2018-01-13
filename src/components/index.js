daa1b417c44d3a090ff2320d92666496430a0807
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-13 19:13:09 -0200

    create component to import another components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import HeaderComponent from './HeaderComponent';
+import FooterComponent from './FooterComponent';
+import CameraComponent from './CameraComponent';
+
+import styles from './styles';
+
+export {HeaderComponent, FooterComponent, CameraComponent, styles};
+
