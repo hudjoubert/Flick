02f6c66d1dc66310cf922ec5b276e891905524e5
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-13 19:10:19 -0200

    create header component

--- src/components/HeaderComponent/index.js
+++ src/components/HeaderComponent/index.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 
-import { Header, Body, Title, Icon, Left } from 'native-base';
+import { Header, Body, Title, Icon, Left, Right } from 'native-base';
+import styles from '../styles';
 
 export default class HeaderComponent extends Component {
     render() {
         return (
-            <Header>
+            <Header style={styles.header}>
                 <Left>
                     <Icon
                         name='add'
+                        style={styles.icons}
                     />
                 </Left>
                 <Body>
                     <Title>Flicky</Title>
                 </Body>
+                <Right>
+                    <Icon name='menu'/>
+                </Right>
 
             </Header>
         );
