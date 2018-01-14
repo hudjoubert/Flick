c4c8478bee5c85f4917852676b714c4c98364e16
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-14 20:48:41 -0200

    organize codes

--- src/components/HeaderComponent/index.js
+++ src/components/HeaderComponent/index.js
@@ -11,7 +11,7 @@ export default class HeaderComponent extends Component {
             <Header style={styles.header}>
                 <Left>
                     {this.props.type === 'Camera' && (
-                        <Button transparent onPress={() => this.props.teste.navigate('Discover')}>
+                        <Button transparent onPress={() => this.props.nav.navigate('Discover')}>
                             <Icon
                                 name='images'
                                 style={styles.icons}
@@ -19,20 +19,29 @@ export default class HeaderComponent extends Component {
                         </Button>
                     )}
                     {this.props.type === 'Contact' && (
-                        <Button transparent onPress={() => this.props.teste.navigate('Home')}>
+                        <Button transparent onPress={() => this.props.nav.navigate('Home')}>
                             <Icon
                                 name='camera'
                                 style={styles.icons}
                             />
                         </Button>
                     )}
+                    {this.props.type === 'Profile' && (
+                        <Button transparent onPress={() => this.props.nav.navigate('Contact')}>
+                            <Icon
+                                name='contacts'
+                                style={styles.icons}
+                            />
+                        </Button>
+                    )}
+
                 </Left>
                 <Body>
                     <Title>Flicky</Title>
                 </Body>
                 <Right>
                     {this.props.type === 'Camera' && (
-                        <Button transparent onPress={() => this.props.teste.navigate('Contact')}>
+                        <Button transparent onPress={() => this.props.nav.navigate('Contact')}>
                             <Icon
                                 name='contacts'
                                 style={styles.icons} />
@@ -40,12 +49,22 @@ export default class HeaderComponent extends Component {
                     )}
 
                     {this.props.type === 'Contact' && (
-                        <Button transparent onPress={() => this.props.teste.navigate('Profile')}>
+                        <Button transparent onPress={() => this.props.nav.navigate('Profile')}>
                             <Icon
                                 name='contact'
                                 style={styles.icons} />
                         </Button>
                     )}
+                    
+                    {this.props.type === 'Discover' && (
+                        <Button transparent onPress={() => this.props.nav.navigate('Home')}>
+                            <Icon
+                                name='camera'
+                                style={styles.icons} />
+                        </Button>
+                    )}
+
+
                 </Right>
 
             </Header>
