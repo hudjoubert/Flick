8b5dcfc35350ce82dcee8081d3a1f9e19c1faaab
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-14 20:46:42 -0200

    update with StoriesView

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,13 +1,15 @@
 import HomeView from '../view/HomeView';
 import ContactView from '../view/ContactView';
 import ProfileView from '../view/ProfileView';
-import DiscoverView from '../view/DiscoverView'
+import DiscoverView from '../view/DiscoverView';
+import StoriesView from '../view/StoriesView';
 
 const Routes = {
     Home: { screen: HomeView},
     Contact: { screen: ContactView},
     Profile: { screen: ProfileView},
     Discover: { screen: DiscoverView},
+    Stories: { screen: StoriesView}
 }
 
 export default Routes;
