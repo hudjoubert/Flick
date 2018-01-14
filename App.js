e96fa9504854c5c217490d05a8ad55ee21fcfda3
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-14 09:38:29 -0200

    create main navigation

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
 import React, { Component } from 'react';
-import {  Platform , StyleSheet} from 'react-native';
-import { Container, Content, Button , Text, Icon, Left,Body,Title} from 'native-base';
-import {HeaderComponent, FooterComponent} from './src/components';
-import HomeView from './src/view/HomeView';
+import { StackNavigator } from 'react-navigation';
+import Routes from './src/Routes/Routes'
 
+const AppNavigator = StackNavigator(Routes, {headerMode: 'none'});
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
-  render() {
-    return (
-      <HomeView></HomeView>
-    );
-  }
+const App = () => {
+  return(
+    <AppNavigator />
+  );
 }
 
-const styles = StyleSheet.create({
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <HomeView></HomeView>
+//     );
+//   }
+// }
+
+export default App;
+
