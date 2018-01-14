b554bf62a57e8ba42f04cc2f8116776850c4b152
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-14 14:26:24 -0200

    update router file

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,9 +1,13 @@
 import HomeView from '../view/HomeView';
 import ContactView from '../view/ContactView';
+import ProfileView from '../view/ProfileView';
+import DiscoverView from '../view/DiscoverView'
 
 const Routes = {
     Home: { screen: HomeView},
-    Contact: { screen: ContactView}
+    Contact: { screen: ContactView},
+    Profile: { screen: ProfileView},
+    Discover: { screen: DiscoverView}
 }
 
 export default Routes;
