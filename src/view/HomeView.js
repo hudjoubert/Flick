cb5d6ac43985e1c969eee9aed2fe7427a3932bea
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-14 14:24:44 -0200

    update HomeView

--- src/view/HomeView.js
+++ src/view/HomeView.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Container, } from 'native-base';
-import { HeaderComponent, CameraComponent } from '../components/index';
+import { HeaderComponent, CameraComponent, ProfileComponent, ContactComponent } from '../components/index';
 
 class HomeView extends Component {
+
+    static navigationOptions = {
+        header: null
+    }
     render() {
+        console.log(this.props);
+        const { navigate } = this.props.navigation;
         return (
             <Container>
-
-                <HeaderComponent />
-                <CameraComponent>
-                </CameraComponent>
+                <HeaderComponent teste={this.props.navigation} type={'Camera'} />
+                <CameraComponent />
             </Container>
         )
     }
