eaf609cc932108bdb9b2f26440895b15fb6c8efc
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-14 14:33:12 -0200

    create StoryComponent and DiscoverView

---
+++ src/components/StoryComponent/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import {
+    Container,
+    Content,
+    Header,
+    Card,
+    CardItem,
+    Thumbnail,
+    Text,
+    Left,
+    Body,
+    Right
+} from 'native-base';
+
+// const cards = [
+//     {
+//         text: 'Card One',
+//         name: 'One',
+//         image: require('./img/swiper-1.png'),
+//     },
+// ];
+
+class StoryComponent extends Component {
+    state = {}
+    render() {
+        return (
+            <Content></Content>
+        );
+    }
+}
+
+export default StoryComponent;
---
+++ src/view/DiscoverView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from 'native-base';
+import { StoryComponent } from '../components/index';
+
+class DiscoverView extends Component {
+    state = {  }
+    render() {
+        return (
+            <StoryComponent />
+        );
+    }
+}
+
+export default DiscoverView;
