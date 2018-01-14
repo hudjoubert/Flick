beac03d00de265ccb84a6b73f12ada023e08b6b4
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-14 14:29:08 -0200

    update camera component to see captured images

--- src/components/CameraComponent/index.js
+++ src/components/CameraComponent/index.js
@@ -3,30 +3,43 @@ import React, { Component } from 'react';
 import {
   Dimensions,
   StyleSheet,
-  Text,
   TouchableHighlight,
-  View
+  View,
+  Image,
 } from 'react-native';
-import { Icon, Container, Header, Left, Body, Right } from 'native-base';
+import { Icon, Container, Header, Left, Body, Right, Thumbnail, Button, Text } from 'native-base';
 import Camera from 'react-native-camera';
 import styles from '../styles';
 
 
 class CameraComponent extends Component {
+  state = {
+    image: ''
+  }
   render() {
     return (
       <Container>
-        <Camera
-          ref={(cam) => { this.camera = cam; }}
-          style={styles.preview}
-          aspect={'fill'}
-          type={'back'}
-          orientation={'portrait'}>
-          <Icon
-            name='radio-button-on'
-            style={styles.capture}
-            onPress={this.takePicture.bind(this)} />
-        </Camera>
+        {this.state.image === '' && (
+          <Camera
+            ref={(cam) => { this.camera = cam; }}
+            style={styles.preview}
+            aspect={'fill'}
+            type={'back'}
+            orientation={'auto'}>
+            <Icon
+              name='radio-button-on'
+              style={styles.capture}
+              onPress={this.takePicture.bind(this)} />
+          </Camera>
+        )}
+        {this.state.image !== '' && (
+          <View>
+            <Image style={styles.myHistory} source={{ uri: this.state.image }} />
+            <Button full onPress={ () => {
+              this.setState({image: ''})
+            }}><Text>Voltar</Text></Button>
+          </View>
+        )}
       </Container>
     );
   }
@@ -35,7 +48,7 @@ class CameraComponent extends Component {
     const options = {};
     //options.location = ...
     this.camera.capture({ metadata: options })
-      .then((data) => console.log(data))
+      .then((data) => this.setState({ image: data.path }))
       .catch(err => console.error(err));
   }
 }
