67278679436116a631a14354e094d8ba09de4ad7
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-14 14:32:13 -0200

    delete footerComponent

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import HeaderComponent from './HeaderComponent';
-import FooterComponent from './FooterComponent';
 import CameraComponent from './CameraComponent';
 import ProfileComponent from './ProfileComponent';
 import ContactComponent from './ContactComponent';
@@ -8,7 +7,6 @@ import styles from './styles';
 
 export {
     HeaderComponent,
-    FooterComponent,
     CameraComponent,
     ProfileComponent,
     ContactComponent,
