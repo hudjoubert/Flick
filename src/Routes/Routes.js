41c215f5484860e430290ee4ad3e1342f1e10b1a
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-14 09:36:17 -0200

    create route config file

---
+++ src/Routes/Routes.js
@@ -0,0 +1,9 @@
+import HomeView from '../view/HomeView';
+import ContactView from '../view/ContactView';
+
+const Routes = {
+    Home: { screen: HomeView},
+    Contact: { screen: ContactView}
+}
+
+export default Routes;
