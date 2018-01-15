edf399351277991461d1ceb0230920a1fddaeab7
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-15 00:21:16 -0200

    create deckSwipper to stories

--- src/view/StoriesView.js
+++ src/view/StoriesView.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import { ImageBackground } from 'react-native';
 import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
 import styles from '../components/styles';
+import { HeaderComponent } from '../components/index';
 // const cards = [
 //   {
 //     text: 'Card One',
@@ -14,27 +15,25 @@ class StoriesView extends Component {
     render() {
         return (
             <Container>
-                <Header />
+                <HeaderComponent nav={this.props.navigation} type={'Stories'}/>
                 <View>
                     <DeckSwiper
                         dataSource={this.props.navigation.state.params.images}
+                        renderEmpty={() =>
+                            <View style={{ alignSelf: "center" }}>
+                              <Text>Over</Text>
+                            </View>}
                         renderItem={item =>
                             <Card style={{ elevation: 3 }}>
                                 <CardItem>
                                     <Left>
-                                        <Thumbnail />
                                         <Body>
-                                            <Text>texte</Text>
-                                            <Text note>NativeBase</Text>
+                                            <Text>Title</Text>
                                         </Body>
                                     </Left>
                                 </CardItem>
                                 <CardItem cardBody>
-                                    <Image style={{ height: 300, flex: 1 }} source={{uri: item}} />
-                                </CardItem>
-                                <CardItem>
-                                    <Icon name="heart" style={{ color: '#ED4A6A' }} />
-                                    <Text>{item.name}</Text>
+                                    <ImageBackground style={{ height: 300, flex: 1 }} source={{uri: item}} ></ImageBackground>
                                 </CardItem>
                             </Card>
                         }
