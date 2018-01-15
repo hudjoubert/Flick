277185cd698a390096e8ea52db4709ed4aa8801d
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-15 00:22:13 -0200

    organize reducer, stores, providers, routes andredux promises

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { StackNavigator } from 'react-navigation';
-import Routes from './src/Routes/Routes'
+import Routes from './src/Routes/Routes';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import reducers from './src/reducers';
 
-const AppNavigator = StackNavigator(Routes, {headerMode: 'none'});
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const store = createStoreWithMiddleware(reducers)
+const AppNavigator = StackNavigator(Routes, { headerMode: 'none' });
 
 const App = () => {
-  return(
-    <AppNavigator />
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   );
 }
 
