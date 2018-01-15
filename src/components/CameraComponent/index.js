e5f5bcf5e780e9acfa78862fc8f993d59e33b30b
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-15 00:17:13 -0200

    remove not used tags from camera

--- src/components/CameraComponent/index.js
+++ src/components/CameraComponent/index.js
@@ -14,35 +14,22 @@ import styles from '../styles';
 
 class CameraComponent extends Component {
   state = {
-    image: '',
     images: []
 
   }
   render() {
-    return (
-      <Container>
-        {this.state.image === '' && (
-          <Camera
-            ref={(cam) => { this.camera = cam; }}
-            style={styles.preview}
-            aspect={'fill'}
-            type={'back'}
-            orientation={'auto'}>
-            <Icon
-              name='radio-button-on'
-              style={styles.capture}
-              onPress={this.takePicture.bind(this)} />
-          </Camera>
-        )}
-        {this.state.image !== '' && (
-          <View>
-            <Image style={styles.myHistory} source={{ uri: this.state.image }} />
-            <Button full onPress={ () => {
-              this.setState({image: ''})
-            }}><Text>Voltar</Text></Button>
-          </View>
-        )}
-      </Container>
+    return ( 
+        <Camera
+          ref={(cam) => { this.camera = cam; }}
+          style={styles.preview}
+          aspect={'fill'}
+          type={'back'}
+          orientation={'auto'}>
+          <Icon
+            name='radio-button-on'
+            style={styles.capture}
+            onPress={this.takePicture.bind(this)} />
+        </Camera>
     );
   }
 
@@ -53,9 +40,9 @@ class CameraComponent extends Component {
       .then((data) => {
         let temp = this.state.images;
         temp.push(data.path);
-        this.setState({images: temp})
-        if(temp.length >= 4)        
-          this.props.nav.navigate('Stories', {images: temp})
+        this.setState({ images: temp })
+        if (temp.length >= 3)
+          this.props.nav.navigate('Stories', { images: temp })
       })
       .catch(err => console.error(err));
   }
