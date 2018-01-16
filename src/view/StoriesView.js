e0918e447b0ab22d3f6855a5f5d7fc5e318ce7b3
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-15 23:27:23 -0200

    resolve problems from StoriesView

--- src/view/StoriesView.js
+++ src/view/StoriesView.js
@@ -22,27 +22,16 @@ class StoriesView extends Component {
 			<Container>
 				<HeaderComponent nav={this.props.navigation} type={'Stories'} />
 				<View>
-					<DeckSwiper>
+					<DeckSwiper
 						dataSource={this.props.navigation.state.params.images}
-						renderEmpty={() =>
-							<View style={{ alignSelf: "center" }}>
-								<Text>Over</Text>
-							</View>}
 						renderItem={item =>
 							<Card style={{ elevation: 3 }}>
-								<CardItem>
-									<Left>
-										<Body>
-											<Text>Title</Text>
-										</Body>
-									</Left>
-								</CardItem>
 								<CardItem cardBody>
-									<ImageBackground style={{ height: 300, flex: 1 }} source={{ uri: item }} ></ImageBackground>
+									<ImageBackground style={{ height: 500, flex: 1 }} source={{ uri: item }} ></ImageBackground>
 								</CardItem>
 							</Card>
 						}
-					</DeckSwiper>
+					/>
 				</View>
 			</Container>
 		);
